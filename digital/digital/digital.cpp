c57d34708b8e7837df97b9e514ab9590bf0852d7
nkasaby <n.kasaby@aucegypt.edu>
2023-02-26 23:45:23 +0200

    adding group members' names
    Nadine and Mariam will add their ID numbers

---
+++ digital/digital/digital.cpp
@@ -0,0 +1,14 @@
+// digital.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// DigitaL Design 1 Project 1 : Quine-McCluskey Logic Minimization
+// Nour Kasaby 900211955
+// Nadine Safwat 900
+// Mariam Elghobary 900
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello:";
+}
+
