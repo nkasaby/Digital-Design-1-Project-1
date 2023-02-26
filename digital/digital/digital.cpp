2de919dcd0568a058861554080e04d3e227b36b4
nkasaby <n.kasaby@aucegypt.edu>
2023-02-27 01:11:57 +0200

    read + validate SOP function
    need to fix to make sure that the function doesn't have ' before a letter or end with a + or have ++

--- digital/digital/digital.cpp
+++ digital/digital/digital.cpp
@@ -5,10 +5,64 @@
 // Mariam Elghobary 900
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include<ctype.h>
 using namespace std;
+
+void reading_func();
+
+
+void validate_alpha(string &str, int n)                    // validating the SoP format only (makes sure function entry contains letters or ' or +) ASSUMING NO SPACES BETWEEN CHARACTERS
+{                                                         
+    bool SOP = false;                                     // ** need to make sure that func doesnt have ' before a letter or end with a +
+    for (int i = 0; i < n; i++) {
+        if (isalpha(str[0]))
+        {
+            if (isalpha(str[i + 1]))
+            {
+                if (str.size()<=10)
+                     SOP = true;
+                else
+                {
+                    cout << "Invalid. Maximum number of vairables is 10. Please enter a new function." << endl;
+                    reading_func();
+                }
+            }
+            else if (str[i + 1] == '+')
+                SOP = true;
+            else if (str[i + 1] == '\'')
+                SOP = true;
+          
+            else
+            {
+                SOP;
+                break;
+            }
+        }
+        else
+            SOP;
+    }
+    if (SOP==true)
+        cout << "SoP format" << endl;
+    else
+    {
+        cout << "Invalid. Not in SoP format, please re-enter your function" << endl;
+        reading_func();
+    }
+}
+
+void reading_func()                            //very basic function that takes SOP function from user and validates it ONLY. (will be improved later)
+{
+    string func;
+    cout << "Please enter a functio as SoP with a maximum of 10 variables." << endl;
+    cin >> func;
+    int n = func.size();
+    validate_alpha(func, n);
+}
 
 int main()
 {
-    cout << "hello:";
+    reading_func();
 }
 
