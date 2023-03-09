3a874c79ac7bf31b606311e2bf26b95eb462d478
NadSafwat <98837821+NadSafwat@users.noreply.github.com>
2023-03-09 15:45:52 +0200

    Update digital.cpp
    Added ID as a test

--- digital/digital/digital.cpp
+++ digital/digital/digital.cpp
@@ -1,7 +1,7 @@
 // digital.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // DigitaL Design 1 Project 1 : Quine-McCluskey Logic Minimization
 // Nour Kasaby 900211955
-// Nadine Safwat 900
+// Nadine Safwat 900212508
 // Mariam Elghobary 900211608
 
 #include <iostream>
